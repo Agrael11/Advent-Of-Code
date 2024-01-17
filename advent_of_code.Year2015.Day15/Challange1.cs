cc9dfefccb3f9e341758986cdd338fcf4d41fb35
Agrael11 <Oliver.Neuschl@gmail.com>
2024-01-17 21:58:46 +0100

    Year 2015, Day 15

---
+++ advent_of_code.Year2015.Day15/Challange1.cs
@@ -0,0 +1,71 @@
+﻿namespace advent_of_code.Year2015.Day15
+{
+    public static class Challange1
+    {
+        private static List<Ingridient> ingridients = [];
+
+        public static long DoChallange(string inputData)
+        {
+            string[] input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            ingridients = [];
+
+            foreach (string line in input)
+            {
+                ingridients.Add(new(line.Split(':')[1]));
+            }
+
+            return GetBestScore(0, []);
+        }
+
+        private static long GetBestScore(int ingridient, List<int> spoons)
+        {
+            if (ingridient == ingridients.Count-1)
+            {
+                int totalSpoons = 0;
+                foreach (int spoon in spoons)
+                {
+                    totalSpoons += spoon;
+                }
+                spoons.Add(100 - totalSpoons);
+
+                int capacity = 0;
+                int durability = 0;
+                int flavor = 0;
+                int texture = 0;
+
+                for (int i = 0; i < spoons.Count; i++)
+                {
+                    capacity += spoons[i] * ingridients[i].Capacity;
+                    durability += spoons[i] * ingridients[i].Durability;
+                    flavor += spoons[i] * ingridients[i].Flavor;
+                    texture += spoons[i] * ingridients[i].Texture;
+                }
+                spoons.RemoveAt(spoons.Count - 1);
+
+                capacity = int.Max(capacity, 0);
+                durability = int.Max(durability, 0);
+                flavor = int.Max(flavor, 0);
+                texture = int.Max(texture, 0);
+
+                return capacity * durability * flavor * texture;
+            }
+            else
+            {
+                int totalSpoons = 0;
+                foreach (int spoon in spoons)
+                {
+                    totalSpoons += spoon;
+                }
+                long bestScore = long.MinValue;
+
+                for (int i = 1; i < 100 - totalSpoons - (ingridients.Count-ingridient); i++)
+                {
+                    spoons.Add(i);
+                    bestScore = long.Max(bestScore, GetBestScore(ingridient + 1, spoons));
+                    spoons.RemoveAt(spoons.Count-1);
+                }
+                return bestScore;
+            }
+        }
+    }
+}
---
+++ advent_of_code.Year2015.Day15/Ingridient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Year2015.Day15
+{
+    internal class Ingridient
+    {
+        public int Capacity { get; set; }
+        public int Durability { get; set; }
+        public int Flavor { get; set; }
+        public int Texture { get; set; }
+        public int Calories { get; set; }
+
+        public Ingridient(string description)
+        {
+            string[] desc = description.Split(',');
+            foreach (string part in desc)
+            {
+                string[] partSplit = part.Trim(' ').Split(" ");
+                switch (partSplit[0])
+                {
+                    case "capacity": Capacity = int.Parse(partSplit[1]); break;
+                    case "durability": Durability = int.Parse(partSplit[1]); break;
+                    case "flavor": Flavor = int.Parse(partSplit[1]); break;
+                    case "texture": Texture = int.Parse(partSplit[1]); break;
+                    case "calories": Calories = int.Parse(partSplit[1]); break;
+                }
+            }
+        }
+    }
+}
