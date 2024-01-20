eae43bb783e5a5287f6abea78d37ef20cf0b03d1
Agrael11 <Oliver.Neuschl@gmail.com>
2024-01-20 12:44:21 +0100

    Year 2015, Day 16

---
+++ advent_of_code.Year2015.Day16/Aunt.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Year2015.Day16
+{
+    internal class Aunt
+    {
+        public enum Property { Children, Cats, Samoyeds, Pomeranians, Akitas, Vizslas, Goldfish, Trees, Cars, Perfumes};
+        private readonly Dictionary<Property, int?> properties = [];
+
+        public Aunt(string infoString)
+        {
+            properties.Clear();
+            foreach (Property property in Enum.GetValues(typeof(Property)))
+            {
+                properties.Add(property, null);
+            }
+            string[] infos = infoString.Split(',');
+            foreach (string info in infos)
+            {
+                string[] data = info.Split(':');
+                switch (data[0].Trim())
+                {
+                    case "children":
+                        properties[Property.Children] = int.Parse(data[1].Trim());
+                        break;
+                    case "cats":
+                        properties[Property.Cats] = int.Parse(data[1].Trim());
+                        break;
+                    case "samoyeds":
+                        properties[Property.Samoyeds] = int.Parse(data[1].Trim());
+                        break;
+                    case "pomeranians":
+                        properties[Property.Pomeranians] = int.Parse(data[1].Trim());
+                        break;
+                    case "akitas":
+                        properties[Property.Akitas] = int.Parse(data[1].Trim());
+                        break;
+                    case "vizslas":
+                        properties[Property.Vizslas] = int.Parse(data[1].Trim());
+                        break;
+                    case "goldfish":
+                        properties[Property.Goldfish] = int.Parse(data[1].Trim());
+                        break;
+                    case "trees":
+                        properties[Property.Trees] = int.Parse(data[1].Trim());
+                        break;
+                    case "cars":
+                        properties[Property.Cars] = int.Parse(data[1].Trim());
+                        break;
+                    case "perfumes":
+                        properties[Property.Perfumes] = int.Parse(data[1].Trim());
+                        break;
+                }
+            }
+        }
+
+        public bool EqualsProperty(Property property, int value, bool part2)
+        {
+            if (part2 && (property == Property.Cats || property == Property.Trees))
+            {
+                return properties[property] == null || properties[property] > value;
+            }
+            else if (part2 && (property == Property.Pomeranians || property == Property.Goldfish))
+            {
+                return properties[property] == null || properties[property] < value;
+            }
+            return properties[property] == null || properties[property] == value;
+        }
+        public bool EqualsProperties(Dictionary<Property,int> properties, bool part2)
+        {
+            foreach (Property key in properties.Keys)
+            {
+                if (!EqualsProperty(key, properties[key], part2)) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ advent_of_code.Year2015.Day16/Challange2.cs
@@ -0,0 +1,34 @@
+﻿namespace advent_of_code.Year2015.Day16
+{
+    public static class Challange2
+    {
+        private static readonly Dictionary<Aunt.Property, int> Prototype = new(){
+                { Aunt.Property.Children, 3 } ,
+                { Aunt.Property.Cats, 7 } ,
+                { Aunt.Property.Samoyeds, 2 } ,
+                { Aunt.Property.Pomeranians, 3 } ,
+                { Aunt.Property.Akitas, 0 } ,
+                { Aunt.Property.Vizslas, 0 } ,
+                { Aunt.Property.Goldfish, 5 } ,
+                { Aunt.Property.Trees, 3 } ,
+                { Aunt.Property.Cars, 2 } ,
+                { Aunt.Property.Perfumes, 1 }
+            };
+
+        public static int DoChallange(string inputData)
+        {
+            string[] input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            foreach (string inputLine in input)
+            {
+                Aunt aunt = new(inputLine[(inputLine.IndexOf(':') + 1)..]);
+                if (aunt.EqualsProperties(Prototype, true))
+                {
+                    return int.Parse(inputLine[(inputLine.IndexOf(' ') + 1)..inputLine.IndexOf(':')]);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
