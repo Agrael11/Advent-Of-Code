7a05e4387d41579f389df962bbd30d8cb6535c7f
Agrael11 <Oliver.Neuschl@gmail.com>
2024-01-26 20:39:14 +0100

    Year 2015, Day 19

---
+++ advent_of_code.Year2015.Day19/Challange1.cs
@@ -0,0 +1,43 @@
+﻿using advent_of_code.Helpers;
+using System.Runtime.CompilerServices;
+
+namespace advent_of_code.Year2015.Day19
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            string[] input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            string inputMolecule = input[^1];
+            Dictionary<string, List<string>> replacements = [];
+
+            foreach (string line in input)
+            {
+                if(string.IsNullOrEmpty(line)) break;
+                string original = line.Split("=>")[0].Trim();
+                string replacement = line.Split("=>")[1].Trim();
+                if (!replacements.TryAdd(original, [replacement]))
+                {
+                    replacements[original].Add(replacement);
+                }
+            }
+
+            HashSet<string> newMolecules = [];
+            
+            foreach (string key in replacements.Keys)
+            {
+                foreach (int index in inputMolecule.IndexesOf(key))
+                {
+                    foreach (string replacement in replacements[key])
+                    {
+                        string newMolecule = inputMolecule[..index] + replacement + inputMolecule[(index+key.Length)..];
+                        newMolecules.Add(newMolecule);
+                    }
+                }
+            }
+
+            return newMolecules.Count;
+        }
+    }
+}
---
+++ advent_of_code.Year2015.Day19/Challange2.cs
@@ -0,0 +1,54 @@
+﻿using advent_of_code.Helpers;
+using System.Linq;
+
+namespace advent_of_code.Year2015.Day19
+{
+    public static class Challange2
+    {
+        private static Dictionary<string, List<string>> replacements = [];
+
+        public static int DoChallange(string inputData)
+        {
+            string[] input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            string startMolecule = input[^1];
+            replacements = [];
+
+            foreach (string line in input)
+            {
+                if (string.IsNullOrEmpty(line)) break;
+                string original = line.Split("=>")[0].Trim();
+                string replacement = line.Split("=>")[1].Trim();
+                if (!replacements.TryAdd(replacement, [original]))
+                {
+                    replacements[replacement].Add(original);
+                }
+            }
+
+            var (type, cost) = PathFinding.DoAStar(startMolecule, (molecule) => { return molecule == "e"; }, NextMolecules,
+                (molecule) => molecule.Count(character => char.IsUpper(character)), 1);
+
+            return cost;
+        }
+
+        public static IEnumerable<(string, int)> NextMolecules(string molecule)
+        { 
+            HashSet<(string, int)> newMolecules = [];
+
+            foreach (string key in replacements.Keys)
+            {
+                foreach (int index in molecule.IndexesOf(key))
+                {
+                    foreach (string replacement in replacements[key])
+                    {
+                        string newMolecule = molecule[..index] + replacement + molecule[(index + key.Length)..];
+
+                        newMolecules.Add((newMolecule, 1));
+                    }
+                }
+            }
+
+            return newMolecules;
+        }
+    }
+}
