da233091f5137b7831518aae28bccde296cc08d5
Agrael11 <Oliver.Neuschl@gmail.com>
2024-01-27 01:16:12 +0100

    Year 2015, Day 20

---
+++ advent_of_code.Year2015.Day20/Challange1.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Quic;
+
+namespace advent_of_code.Year2015.Day20
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = int.Parse(inputData.Replace("\r", "").TrimEnd('\n'));
+
+            var house = 1;
+
+            while (SumDivisors(house) * 10 < input)
+            {
+                house++;
+            }
+
+            return house;
+        }
+
+        private static int SumDivisors(int number)
+        {
+            var divisorsSum = 0;
+
+            for (var i = 1; i <= (int)Math.Sqrt(number); i++)
+            {
+                var quotient = number / i;
+                var remainder = number % i;
+                if (remainder == 0)
+                {
+                    if (i != quotient) divisorsSum += quotient;
+                    divisorsSum += i;
+                }
+            }
+
+            return divisorsSum;
+        }
+    }
+}
---
+++ advent_of_code.Year2015.Day20/Challange2.cs
@@ -0,0 +1,37 @@
+﻿namespace advent_of_code.Year2015.Day20
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = int.Parse(inputData.Replace("\r", "").TrimEnd('\n'));
+
+            var house = 1;
+
+            while (SumDivisors(house) * 11 < input)
+            {
+                house++;
+            }
+
+            return house;
+        }
+
+        private static int SumDivisors(int number)
+        {
+            var divisorsSum = 0;
+
+            for (var i = 1; i <= (int)Math.Sqrt(number); i++)
+            {
+                var quotient = number / i;
+                var remainder = number % i;
+                if (remainder == 0)
+                {
+                    if (i != quotient && i <= 50) divisorsSum += quotient;
+                    if (quotient <= 50) divisorsSum += i;
+                }
+            }
+
+            return divisorsSum;
+        }
+    }
+}
