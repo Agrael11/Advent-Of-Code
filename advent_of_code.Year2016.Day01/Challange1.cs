9bf0ea1bdfb77c5da3674656f25f26aba8421851
Agrael11 <Oliver.Neuschl@gmail.com>
2024-02-03 15:53:33 +0100

    Year 2016, Day 1

---
+++ advent_of_code.Year2016.Day01/Challange1.cs
@@ -0,0 +1,28 @@
+﻿namespace advent_of_code.Year2016.Day01
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(',');
+            var currentDir = 0;
+            var horizontal = 0;
+            var vertical = 0;
+            foreach (var direction in input)
+            {
+                var dir = direction.Trim();
+                if (dir[0] == 'L') currentDir--;
+                else currentDir++;
+                if (currentDir > 3) currentDir -= 4;
+                else if (currentDir < 0) currentDir += 4;
+
+                if (currentDir == 0) vertical += int.Parse(dir[1..]);
+                else if (currentDir == 2) vertical -= int.Parse(dir[1..]);
+                else if (currentDir == 1) horizontal += int.Parse(dir[1..]);
+                else if (currentDir == 3) horizontal -= int.Parse(dir[1..]);
+            }
+
+            return Math.Abs(vertical)+Math.Abs(horizontal);
+        }
+    }
+}
---
+++ advent_of_code.Year2016.Day01/Challange2.cs
@@ -0,0 +1,44 @@
+﻿namespace advent_of_code.Year2016.Day01
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(',');
+            var currentDir = 0;
+            var currentX = 0;
+            var currentY = 0;
+            var visited = new HashSet<(int, int)> ();
+            visited.Add((currentX, currentY));
+            foreach (var direction in input)
+            {
+                var dir = direction.Trim();
+                if (dir[0] == 'L') currentDir--;
+                else currentDir++;
+                if (currentDir > 3) currentDir -= 4;
+                else if (currentDir < 0) currentDir += 4;
+
+                var dist = int.Parse(dir[1..]);
+                
+                for (var i = 0; i < dist; i++)
+                {
+                    if (currentDir == 0) currentX++;
+                    else if (currentDir == 2) currentX--;
+                    else if (currentDir == 1) currentY++;
+                    else if (currentDir == 3) currentY--;
+
+                    if (visited.Contains((currentX, currentY)))
+                    {
+                        return Math.Abs(currentX)+Math.Abs(currentY);
+                    }
+                    else
+                    {
+                        visited.Add((currentX, currentY));
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
