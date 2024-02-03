11689a28c660f37b7468fc00a219905ee44d3e59
Agrael11 <Oliver.Neuschl@gmail.com>
2024-02-03 13:48:27 +0100

    Year 2015, Day 24

---
+++ advent_of_code.Year2015.Day24/Challange1.cs
@@ -0,0 +1,39 @@
+﻿using advent_of_code.Helpers;
+using System.Security.AccessControl;
+
+namespace advent_of_code.Year2015.Day24
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var packages = new List<int>();
+            foreach (var line in input)
+            {
+                packages.Add(int.Parse(line));
+            }
+
+            var target = packages.Sum() / 3;
+
+            var foundSize = -1;
+            var minProduct = long.MaxValue;
+
+            foreach (var item in packages.YieldCombinations())
+            {
+                var size = item.Count;
+                if (foundSize != -1 && size > foundSize)
+                    break;
+
+                if (item.Sum() == target)
+                {
+                    foundSize = size;
+                    minProduct = Math.Min(minProduct, item.Aggregate((long)1, (acc, val) => acc * val));
+                }
+            }
+
+            return minProduct;
+        }
+    }
+}
---
+++ advent_of_code.Year2015.Day24/Challange2.cs
@@ -0,0 +1,38 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code.Year2015.Day24
+{
+    public static class Challange2
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var packages = new List<int>();
+            foreach (var line in input)
+            {
+                packages.Add(int.Parse(line));
+            }
+
+            var target = packages.Sum() / 4;
+
+            var foundSize = -1;
+            var minProduct = long.MaxValue;
+
+            foreach (var item in packages.YieldCombinations())
+            {
+                var size = item.Count;
+                if (foundSize != -1 && size > foundSize)
+                    break;
+
+                if (item.Sum() == target)
+                {
+                    foundSize = size;
+                    minProduct = Math.Min(minProduct, item.Aggregate((long)1, (acc, val) => acc * val));
+                }
+            }
+
+            return minProduct;
+        }
+    }
+}
