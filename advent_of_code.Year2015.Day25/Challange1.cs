4304873cd2e37d54f93e26105b79ff8812f692f7
Agrael11 <Oliver.Neuschl@gmail.com>
2024-02-03 14:47:15 +0100

    Year 2015, Day 25 -Year 2015 Done

---
+++ advent_of_code.Year2015.Day25/Challange1.cs
@@ -0,0 +1,35 @@
+﻿using advent_of_code.Helpers;
+
+namespace advent_of_code.Year2015.Day25
+{
+    public static class Challange1
+    {
+        private static readonly long StartCode = 20151125;
+        private static readonly long Multiplier = 252533;
+        private static readonly long Divider = 33554393;
+
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n');
+
+            var row = int.Parse(input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[15][..^1]);
+            var column = int.Parse(input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[17][..^1]);
+            var index = GetIndex(row, column);
+
+            var code = StartCode;
+
+            for (var i = 1; i < index; i++)
+            {
+                code *= Multiplier;
+                code %= Divider;
+            }
+
+            return code;
+        }
+
+        public static int GetIndex(int row, int column)
+        {
+            return 1 + MathHelpers.Sum(1, row - 1) + MathHelpers.Sum(row + 1, row + column - 1);
+        }
+    }
+}
---
+++ advent_of_code.Year2015.Day25/Challange2.cs
@@ -0,0 +1,12 @@
+﻿namespace advent_of_code.Year2015.Day25
+{
+    public static class Challange2
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static string DoChallange(string inputData)
+        {
+            return "Look into fuel tank, one star sits on bottom.";
+        }
+#pragma warning restore IDE0060 // Remove unused parameter
+    }
+}
