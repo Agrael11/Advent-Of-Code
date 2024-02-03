1670b440d3b155534e6e99a47eb553b0924cd915
Agrael11 <Oliver.Neuschl@gmail.com>
2024-02-03 13:48:12 +0100

    Split extensions

---
+++ advent_of_code.Helpers/ExtensionsIndexOf.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Helpers
+{
+    public static class ExtensionsIndexOf
+    {
+        public static IEnumerable<int> IndexesOf(this string source, string value, StringComparison comparison = StringComparison.Ordinal)
+        {
+            for (var i = source.IndexOf(value, 0, comparison); i > -1; i = source.IndexOf(value, i + 1, comparison))
+            {
+                yield return i;
+            }
+        }
+        public static IEnumerable<int> IndexesOf(this string source, char value)
+        {
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (source[i] == value)
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        public static IEnumerable<int> LastIndexesOf(this string source, string value, StringComparison comparison = StringComparison.Ordinal)
+        {
+            for (var i = source.LastIndexOf(value, 0, comparison); i > -1; i = source.LastIndexOf(value, i - 1, comparison))
+            {
+                yield return i;
+                if (i == 0) break;
+            }
+        }
+
+        public static IEnumerable<int> LastIndexesOf(this string source, char value)
+        {
+            for (var i = source.Length - 1; i >= 0; i--)
+            {
+                if (source[i] == value)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
--- advent_of_code.Helpers/ExtensionsPermutations.cs
+++ advent_of_code.Helpers/ExtensionsPermutations.cs
@@ -1,6 +1,8 @@
-﻿namespace advent_of_code.Helpers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace advent_of_code.Helpers
 {
-    public static class Extensions
+    public static class ExtensionsPermutations
     {
         public static List<List<T>> Permutate<T>(this IEnumerable<T> source)
         {
@@ -57,44 +59,6 @@
             foreach (var v in Generate(0).Cast<List<T>>())
             {
                 yield return v;
-            }
-        }
-
-        public static IEnumerable<int> IndexesOf(this string source, string value, StringComparison comparison = StringComparison.Ordinal)
-        {
-            for (var i = source.IndexOf(value, 0, comparison); i > -1; i = source.IndexOf(value, i + 1, comparison))
-            {
-                yield return i;
-            }
-        }
-        public static IEnumerable<int> IndexesOf(this string source, char value)
-        {
-            for (var i = 0; i < source.Length; i++)
-            {
-                if (source[i] == value)
-                {
-                    yield return i;
-                }
-            }
-        }
-
-        public static IEnumerable<int> LastIndexesOf(this string source, string value, StringComparison comparison = StringComparison.Ordinal)
-        {
-            for (var i = source.LastIndexOf(value, 0, comparison); i > -1; i = source.LastIndexOf(value, i - 1, comparison))
-            {
-                yield return i;
-                if (i == 0) break;
-            }
-        }
-
-        public static IEnumerable<int> LastIndexesOf(this string source, char value)
-        {
-            for (var i = source.Length - 1; i >= 0; i--)
-            {
-                if (source[i] == value)
-                {
-                    yield return i;
-                }
             }
         }
     }
