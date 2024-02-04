b07dc0a6605c33ecd5dfbbbaceee1850599a2fe0
Agrael11 <Oliver.Neuschl@gmail.com>
2024-02-04 21:18:15 +0100

    Year 2016, Day 3

---
+++ advent_of_code.Year2016.Day03/Challange1.cs
@@ -0,0 +1,24 @@
+﻿namespace advent_of_code.Year2016.Day03
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var validTrianles = 0;
+            foreach (var line in input)
+            {
+                var triangleData = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                validTrianles += TriangleValid(int.Parse(triangleData[0]), int.Parse(triangleData[1]), int.Parse(triangleData[2])) ? 1 : 0;
+            }
+
+            return validTrianles;
+        }
+
+        private static bool TriangleValid(int sideA, int sideB, int sideC)
+        {
+            return (sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA);
+        }
+    }
+}
---
+++ advent_of_code.Year2016.Day03/Challange2.cs
@@ -0,0 +1,27 @@
+﻿namespace advent_of_code.Year2016.Day03
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var validTrianles = 0;
+            for (var lineIndex = 0; lineIndex < input.Length; lineIndex += 3)
+            {
+                var line1Split = input[lineIndex].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var line2Split = input[lineIndex + 1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var line3Split = input[lineIndex + 2].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                validTrianles += TriangleValid(int.Parse(line1Split[0]), int.Parse(line2Split[0]), int.Parse(line3Split[0])) ? 1 : 0;
+                validTrianles += TriangleValid(int.Parse(line1Split[1]), int.Parse(line2Split[1]), int.Parse(line3Split[1])) ? 1 : 0;
+                validTrianles += TriangleValid(int.Parse(line1Split[2]), int.Parse(line2Split[2]), int.Parse(line3Split[2])) ? 1 : 0;   
+            }
+            return validTrianles;
+        }
+
+        private static bool TriangleValid(int sideA, int sideB, int sideC)
+        {
+            return (sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA);
+        }
+    }
+}
