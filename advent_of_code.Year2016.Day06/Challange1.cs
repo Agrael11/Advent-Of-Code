3c8f6393e6638dcbd6b4c2f5ee560fb91f635411
Tachi <Oliver.Neuschl@gmail.com>
2024-03-21 11:14:50 +0100

    Year 2016, Day 6

---
+++ advent_of_code.Year2016.Day06/Challange1.cs
@@ -0,0 +1,30 @@
+﻿namespace advent_of_code.Year2016.Day06
+{
+    public static class Challange1
+    {
+        public static string DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var corrected = "";
+
+            for (var i = 0; i < input[0].Length; i++)
+            {
+                var chars = new Dictionary<char, int>();
+                var mostOccuringChar = '\0';
+                var mostOccuringTimes = 0;
+                foreach (var message in input)
+                {
+                    chars.TryGetValue(message[i], out var count);
+                    chars[message[i]] = count + 1;
+                    if (count + 1 > mostOccuringTimes)
+                    {
+                        mostOccuringTimes = count + 1;
+                        mostOccuringChar = message[i];
+                    }
+                }
+                corrected += mostOccuringChar;
+            }
+            return corrected;
+        }
+    }
+}
---
+++ advent_of_code.Year2016.Day06/Challange2.cs
@@ -0,0 +1,23 @@
+﻿namespace advent_of_code.Year2016.Day06
+{
+    public static class Challange2
+    {
+        public static string DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var corrected = "";
+
+            for (var i = 0; i < input[0].Length; i++)
+            {
+                var chars = new Dictionary<char, int>();
+                foreach (var message in input)
+                {
+                    chars.TryGetValue(message[i], out var count);
+                    chars[message[i]] = count + 1;
+                }
+                corrected += chars.MinBy(t => t.Value).Key;
+            }
+            return corrected;
+        }
+    }
+}
