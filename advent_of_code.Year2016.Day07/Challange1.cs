5784d8e5c6b5ae022c8cf39b21bfef29935a73e2
Tachi <Oliver.Neuschl@gmail.com>
2024-03-21 11:48:24 +0100

    Year 2016, Day 7

---
+++ advent_of_code.Year2016.Day07/Challange1.cs
@@ -0,0 +1,51 @@
+﻿namespace advent_of_code.Year2016.Day07
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var count = 0;
+
+            foreach (var address in input)
+            {
+                count += CheckTLS(address);
+            }
+
+            return count;
+        }
+
+        public static int CheckTLS(string address)
+        {
+            var hyperNetState = false;
+            var foundAbba = 0;
+            for (var i = 0; i < address.Length - 3; i++)
+            {
+                if (address[i] == '[')
+                {
+                    hyperNetState = true;
+                    continue;
+                }
+
+                if (address[i] == ']')
+                {
+                    hyperNetState = false;
+                    continue;
+                }
+
+                if (address[i] == address[i + 3] && address[i + 1] == address[i + 2] && address[i] != address[i + 1])
+                {
+                    if (hyperNetState)
+                    {
+                        return 0;
+                    }
+                    
+                    foundAbba = 1;
+                }
+            }
+
+            return foundAbba;
+        }
+    }
+}
---
+++ advent_of_code.Year2016.Day07/Challange2.cs
@@ -0,0 +1,56 @@
+﻿namespace advent_of_code.Year2016.Day07
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var count = 0;
+
+            foreach (var address in input)
+            {
+                count += CheckSSL(address);
+            }
+
+            return count;
+        }
+
+        public static int CheckSSL(string address)
+        {
+            var hyperNetState = false;
+            var abas = new HashSet<string>();
+            var babs = new HashSet<string>();
+            for (var i = 0; i < address.Length - 2; i++)
+            {
+                if (address[i] == '[')
+                {
+                    hyperNetState = true;
+                    continue;
+                }
+
+                if (address[i] == ']')
+                {
+                    hyperNetState = false;
+                    continue;
+                }
+
+                if (address[i] == address[i + 2] && address[i] != address[i + 1] && (address[i + 1] != '[' || address[i + 1] != ']'))
+                {
+                    if (hyperNetState)
+                    {
+                        if (abas.Contains(address[i + 1] + "" + address[i])) return 1;
+                        babs.Add(address[i + 1] + "" + address[i]);
+                    }
+                    else
+                    {
+                        if (babs.Contains(address[i] + "" + address[i + 1])) return 1;
+                        abas.Add(address[i] + "" + address[i + 1]);
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
