ce84654dd5f94ebaa7f78482e09010e1e5110262
Tachi <Oliver.Neuschl@gmail.com>
2024-03-21 22:00:57 +0100

    Year 2016, Day 9

---
+++ advent_of_code.Year2016.Day09/Challange1.cs
@@ -0,0 +1,30 @@
+﻿namespace advent_of_code.Year2016.Day09
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n');
+                
+            var length = 0L;
+
+            for (var i = 0; i < input.Length;)
+            {
+                if (input[i] == '(')
+                {
+                    var marker = input[(i+1)..input.IndexOf(')', i + 1)];
+                    var markerData = marker.Split('x');
+                    i = i + 2 + marker.Length + int.Parse(markerData[0]);
+                    length += int.Parse(markerData[0]) * int.Parse(markerData[1]);
+                }
+                else
+                {
+                    length++;
+                    i++;
+                }
+            }
+
+            return length;
+        }
+    }
+}
---
+++ advent_of_code.Year2016.Day09/Challange2.cs
@@ -0,0 +1,37 @@
+﻿namespace advent_of_code.Year2016.Day09
+{
+    public static class Challange2
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n');
+
+            return Decompress(input);
+        }
+
+        public static long Decompress(string input)
+        {
+            var length = 0L;
+
+            for (var i = 0; i < input.Length;)
+            {
+                if (input[i] == '(')
+                {
+                    var marker = input[(i + 1)..input.IndexOf(')', i + 1)];
+                    var markerData = marker.Split('x');
+                    i = i + 2 + marker.Length;
+                    var markedInput = input[i..(i + int.Parse(markerData[0]))];
+                    i += int.Parse(markerData[0]);
+                    length += Decompress(markedInput) * int.Parse(markerData[1]);
+                }
+                else
+                {
+                    length++;
+                    i++;
+                }
+            }
+
+            return length;
+        }
+    }
+}
