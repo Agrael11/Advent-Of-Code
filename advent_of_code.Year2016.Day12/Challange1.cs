f30148d95e779cca8079d6a9df611ea4b9c18ddc
Tachi <Oliver.Neuschl@gmail.com>
2024-03-26 00:34:14 +0100

    Year 2016, Day 12 *fixed possible error

--- advent_of_code.Year2016.Day12/Challange1.cs
+++ advent_of_code.Year2016.Day12/Challange1.cs
@@ -4,7 +4,7 @@
     {
         public static int DoChallange(string inputData)
         {
-            var input = inputData.Replace("\r", "").Replace(",", "").Replace(".", "").TrimEnd('\n').Split("\n");
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n");
 
             var cpu = new CPU(0, 0, 0, 0, input);
             cpu.Run();
