3377a308b45d567e5f00ab4a00d385bfdf3b4b36
Tachi <Oliver.Neuschl@gmail.com>
2024-03-26 14:01:06 +0100

    Year 2016, Day 15

---
+++ advent_of_code.Year2016.Day15/Challange2.cs
@@ -0,0 +1,43 @@
+﻿namespace advent_of_code.Year2016.Day15
+{
+    public static class Challange2
+    {
+        public static List<(int count, int start)> disks = new List<(int count, int start)>();
+
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").Replace("#", "").Replace(".", "").TrimEnd('\n').Split('\n');
+            foreach (var line in input)
+            {
+                var diskInfo = line.Split(' ');
+                var positionCount = int.Parse(diskInfo[3]);
+                var startPosition = int.Parse(diskInfo[^1]);
+                disks.Add((positionCount, startPosition));
+            }
+            disks.Add((11, 0));
+
+            var startTime = 0;
+            while (!FallsThrough(startTime))
+            {
+                startTime++;
+            }
+
+            disks.Clear();
+            return startTime;
+        }
+
+        public static bool FallsThrough(int time)
+        {
+            for (var i = 0; i < disks.Count; i++)
+            {
+                if (!Safe(time, i)) return false;
+            }
+            return true;
+        }
+
+        public static bool Safe(int time, int disk)
+        {
+            return ((time + disk + 1 + disks[disk].start) % disks[disk].count) == 0;
+        }
+    }
+}
