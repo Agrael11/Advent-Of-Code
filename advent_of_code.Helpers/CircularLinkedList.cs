657ac460e68eddad359ebecf4029d115b104c40b
Tachi <Oliver.Neuschl@gmail.com>
2024-03-27 16:48:19 +0100

    Added CircularLinkedList system

---
+++ advent_of_code.Helpers/CircularLinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Helpers
+{
+    public class CircularLinkedList<T> where T : notnull
+    {
+        private CircularListItem<T>? root;
+        public int Length { get; private set; }
+        private readonly Dictionary<T, CircularListItem<T>> list;
+
+
+        public  CircularLinkedList()
+        {
+            root = null;
+            list = new Dictionary<T, CircularListItem<T>>();
+            Length = 0;
+        }
+
+        public T GetRoot()
+        {
+            if (root is null) throw new Exception("Tried to get from empty list");
+            return root.Item;
+        }
+
+        public void Add(T item)
+        {
+            var newItem = new CircularListItem<T>(item);
+            if (Length > 0 && root is not null)
+            {
+                newItem.PreviousItem = root.PreviousItem;
+                newItem.NextItem = root;
+                newItem.PreviousItem.NextItem = newItem;
+                newItem.NextItem.PreviousItem = newItem;
+            }
+            else
+            {
+                root = newItem;
+            }
+            list.Add(item, newItem);
+            Length++;
+        }
+
+        public T GetAfter(T item)
+        {
+            return list[item].NextItem.Item;
+        }
+
+        public T GetBefore(T item)
+        {
+            return list[item].PreviousItem.Item;
+        }
+
+        public void Remove(T item)
+        {
+            if (Length == 0) throw new Exception("Tried to remove from empty list");
+            
+            list[item].PreviousItem.NextItem = list[item].NextItem;
+            list[item].NextItem.PreviousItem = list[item].PreviousItem;
+            if (root == list[item]) root = list[item].NextItem;
+            list.Remove(item);
+            Length--;
+        }
+    }
+}
---
+++ advent_of_code.Helpers/CircularListItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Helpers
+{
+    internal class CircularListItem<T>
+    {
+        public T Item { get; set; }
+        public CircularListItem<T> PreviousItem;
+        public CircularListItem<T> NextItem;
+
+        public CircularListItem(T item)
+        {
+            Item = item;
+            PreviousItem = this;
+            NextItem = this;
+        }
+
+        public override string ToString()
+        {
+            return $"{Item}, {{{PreviousItem.Item}-{NextItem.Item}}}";
+        }
+    }
+}
