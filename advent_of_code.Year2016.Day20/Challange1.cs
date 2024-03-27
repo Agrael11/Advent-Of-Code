70303a5a6ccdebcc653334f53cf8e0862fff5f78
Tachi <Oliver.Neuschl@gmail.com>
2024-03-27 22:55:54 +0100

    Year 2016, Day 20

---
+++ advent_of_code.Year2016.Day20/Challange1.cs
@@ -0,0 +1,30 @@
+﻿namespace advent_of_code.Year2016.Day20
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var ranges = new List<Helpers.Range>();
+
+            foreach (var ipRange in input)
+            {
+                var rangeStrings = ipRange.Split('-');
+                var lower = long.Parse(rangeStrings[0]);
+                var higher = long.Parse(rangeStrings[1]);
+                ranges.Add(new Helpers.Range(lower, higher));
+            }
+
+            ranges = Helpers.Range.TryCombine(ranges);
+
+            var lowest = 0L;
+            foreach (var range in ranges)
+            {
+                if (range.InRange(lowest)) lowest = range.End + 1;
+            }
+
+            return lowest;
+        }
+    }
+}
---
+++ advent_of_code.Year2016.Day20/Challange2.cs
@@ -0,0 +1,32 @@
+﻿namespace advent_of_code.Year2016.Day20
+{
+    public static class Challange2
+    {
+        private static readonly long maxValue = 4294967295;
+
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var ranges = new List<Helpers.Range>();
+
+            foreach (var ipRange in input)
+            {
+                var rangeStrings = ipRange.Split('-');
+                var lower = long.Parse(rangeStrings[0]);
+                var higher = long.Parse(rangeStrings[1]);
+                ranges.Add(new Helpers.Range(lower, higher));
+            }
+            ranges = Helpers.Range.TryCombine(ranges);
+
+            var result = maxValue + 1;
+
+            foreach (var range in ranges)
+            {
+                result -= range.Length;
+            }
+
+            return result;
+        }
+    }
+}
