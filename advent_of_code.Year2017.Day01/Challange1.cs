c63be12ac4a1a5e94c765bd27bd7947d527959ff
Tachi <Oliver.Neuschl@gmail.com>
2024-04-07 23:00:00 +0200

    Year 2017, Day 1

---
+++ advent_of_code.Year2017.Day01/Challange1.cs
@@ -0,0 +1,18 @@
+﻿namespace advent_of_code.Year2017.Day01
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n');
+            var capcha = (input[0] == input[^1])?input[0]-'0':0;
+
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i+1]) capcha += input[i]-'0';
+            }
+
+            return capcha;
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day01/Challange2.cs
@@ -0,0 +1,18 @@
+﻿namespace advent_of_code.Year2017.Day01
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n');
+            var capcha = (input[0] == input[input.Length / 2]) ? input[0] - '0' : 0;
+
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[(i + input.Length/2)%input.Length]) capcha += input[i] - '0';
+            }
+
+            return capcha;
+        }
+    }
+}
