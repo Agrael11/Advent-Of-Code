49d0f18fa1d35e28ae04ae188f754f5974556a5c
Tachi <Oliver.Neuschl@gmail.com>
2024-04-13 19:38:57 +0200

    Year 2017, Day 2

---
+++ advent_of_code.Year2017.Day02/Challange1.cs
@@ -0,0 +1,26 @@
+﻿namespace advent_of_code.Year2017.Day02
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var checksum = 0;
+
+            foreach (var row in input)
+            {
+                var min = int.MaxValue;
+                var max = int.MinValue;
+                foreach (var cell in row.Split('\t'))
+                {
+                    var num = int.Parse(cell);
+                    min = int.Min(min, num);
+                    max = int.Max(max, num);
+                }
+                checksum += max - min;
+            }
+
+            return checksum;
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day02/Challange2.cs
@@ -0,0 +1,40 @@
+﻿namespace advent_of_code.Year2017.Day02
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var result = 0;
+
+            foreach (var row in input)
+            {
+                result += FindResult(row);
+            }
+
+            return result;
+        }
+
+        private static int FindResult(string row)
+        {
+            var numbers = row.Split('\t');
+            for (var NumIndex1 = 0; NumIndex1 < numbers.Length - 1; NumIndex1++)
+            {
+                var num1 = int.Parse(numbers[NumIndex1]);
+                for (var NumIndex2 = NumIndex1 + 1; NumIndex2 < numbers.Length; NumIndex2++)
+                {
+                    var num2 = int.Parse(numbers[NumIndex2]);
+                    if (num1 % num2 == 0)
+                    {
+                        return num1 / num2;
+                    }
+                    if (num2 % num1 == 0)
+                    {
+                        return num2 / num1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
