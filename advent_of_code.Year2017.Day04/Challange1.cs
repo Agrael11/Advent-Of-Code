ce2dd6aa204aeddda726770034d47e9d874ac886
Tachi <Oliver.Neuschl@gmail.com>
2024-05-09 23:43:35 +0200

    Year 2017, Day 4

---
+++ advent_of_code.Year2017.Day04/Challange1.cs
@@ -0,0 +1,29 @@
+﻿namespace advent_of_code.Year2017.Day04
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var valid = 0;
+            
+            foreach (var passphrase in input)
+            {
+                if (PassphraseValid(passphrase)) valid++;
+            }
+
+            return valid;
+        }
+
+        private static bool PassphraseValid(string passphrase)
+        {
+            var passwords = new HashSet<string>();
+            foreach (var password in passphrase.Split(' '))
+            {
+                if (passwords.Contains(password)) return false;
+                passwords.Add(password); 
+            }
+            return true;
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day04/Challange2.cs
@@ -0,0 +1,36 @@
+﻿namespace advent_of_code.Year2017.Day04
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var valid = 0;
+
+            foreach (var passphrase in input)
+            {
+                if (PassphraseValid(passphrase)) valid++;
+            }
+
+            return valid;
+        }
+
+        private static bool PassphraseValid(string passphrase)
+        {
+            var passwords = new HashSet<string>();
+            var anagrams = new HashSet<string>();
+
+            foreach (var password in passphrase.Split(' '))
+            {
+                var anagramArray = password.ToCharArray();
+                Array.Sort(anagramArray);
+                var anagram = new string(anagramArray);
+
+                if (passwords.Contains(password) || anagrams.Contains(anagram)) return false;
+                passwords.Add(password);
+                anagrams.Add(anagram);
+            }
+            return true;
+        }
+    }
+}
