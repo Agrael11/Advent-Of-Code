e72de2c7bba6496be6745e29cf9318c1ee580c3e
Tachi <Oliver.Neuschl@gmail.com>
2024-05-10 08:50:34 +0200

    Year 2017, Day 5

---
+++ advent_of_code.Year2017.Day05/Challange1.cs
@@ -0,0 +1,28 @@
+﻿namespace advent_of_code.Year2017.Day05
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var instructions = new List<int>();
+
+            foreach (var line in input)
+            {
+                instructions.Add(int.Parse(line));
+            }
+
+            var PC = 0;
+            var steps = 0;
+
+            while (PC < instructions.Count)
+            {
+                instructions[PC]++;
+                PC += instructions[PC] - 1;
+                steps++;
+            }
+
+            return steps;
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day05/Challange2.cs
@@ -0,0 +1,36 @@
+﻿namespace advent_of_code.Year2017.Day05
+{
+    public static class Challange2
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            var instructions = new List<int>();
+
+            foreach (var line in input)
+            {
+                instructions.Add(int.Parse(line));
+            }
+
+            var PC = 0;
+            var steps = 0L;
+
+            while (PC < instructions.Count)
+            {
+                if (instructions[PC] >= 3)
+                {
+                    instructions[PC]--;
+                    PC += instructions[PC] + 1;
+                }
+                else
+                {
+                    instructions[PC]++;
+                    PC += instructions[PC] - 1;
+                }
+                steps++;
+            }
+
+            return steps;
+        }
+    }
+}
