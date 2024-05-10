3a9ebc16569b111e692e4d7a7b4a6bbcd31ed19d
Tachi <Oliver.Neuschl@gmail.com>
2024-05-10 19:17:21 +0200

    Year 2017, Day 9

---
+++ advent_of_code.Year2017.Day09/Challange1.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace advent_of_code.Year2017.Day09
+{
+    public static class Challange1
+    {
+        public static readonly char[] AllowedCharacters = new char[] { '<', '>', '{', '}', '!'};
+
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n');
+
+            var builder = new StringBuilder();
+            var inGarbage = false;
+
+            for (var i = 0; i < input.Length; i++) 
+            {
+                var c = input[i];
+                if (AllowedCharacters.Contains(c))
+                {
+                    switch (c)
+                    {
+                        case '!':
+                            i++;
+                            break;
+                        case '<':
+                            inGarbage = true;
+                            break;
+                        case '>':
+                            inGarbage = false;
+                            break;
+                        default:
+                            if (!inGarbage) builder.Append(c);
+                            break;
+                    }
+                }
+            }
+
+            var total = 0;
+            var level = 0;
+
+            foreach (var c in builder.ToString())
+            {
+                if (c == '{')
+                {
+                    level++;
+                    total += level;
+                }
+                if (c == '}')
+                {
+                    level--;
+                }
+            }
+
+            return total;
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day09/Challange2.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace advent_of_code.Year2017.Day09
+{
+    public static class Challange2
+    {
+        public static readonly char[] AllowedCharacters = new char[] { '<', '>', '{', '}', '!' };
+
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n');
+
+            var builder = new StringBuilder();
+            var inGarbage = false;
+            var garbage = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+                switch (c)
+                {
+                    case '!':
+                        i++;
+                        break;
+                    case '<':
+                        if (inGarbage) garbage++;
+                        else inGarbage = true;
+                        break;
+                    case '>':
+                        inGarbage = false;
+                        break;
+                    default:
+                        if (inGarbage) garbage++;
+                        break;
+                }
+            }
+
+            return garbage;
+        }
+    }
+}
