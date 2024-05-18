7cfdbeea6397bf2ae63da2217fa24511c9f41220
Tachi <Oliver.Neuschl@gmail.com>
2024-05-18 23:28:24 +0200

    Year 2017, Day 16

---
+++ advent_of_code.Year2017.Day16/Challange1.cs
@@ -0,0 +1,68 @@
+﻿namespace advent_of_code.Year2017.Day16
+{
+    public static class Challange1
+    {
+        public static string DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(',');
+
+            var state = "abcdefghijklmnop".ToCharArray();
+
+            foreach (var line in input)
+            {
+                var data = line[1..].Split('/');
+                switch (line[0])
+                {
+                    case 's':
+                        var len = int.Parse(data[0]);
+                        Spin(ref state, len);
+                        break;
+                    case 'x':
+                        var pos1 = int.Parse(data[0]);
+                        var pos2 = int.Parse(data[1]);
+                        (state[pos2], state[pos1]) = (state[pos1], state[pos2]);
+                        break;
+                    case 'p':
+                        var found = false;
+                        for (var i = 0; i < state.Length; i++)
+                        {
+                            if (state[i] == data[0][0])
+                            {
+                                state[i] = data[1][0];
+                                if (found) break;
+                                found = true;
+                            }
+                            else if (state[i] == data[1][0])
+                            {
+                                state[i] = data[0][0];
+                                if (found) break;
+                                found = true;
+                            }
+                        }
+                        break;
+                }
+            }
+
+            return new string(state);
+        }
+
+        private static void Spin(ref char[] array, int length)
+        {
+            var n = array.Length;
+            length %= n;
+            Reverse(ref array, 0, n - 1);
+            Reverse(ref array, 0, length - 1);
+            Reverse(ref array, length, n - 1);
+        }
+
+        private static void Reverse(ref char[] array, int start, int end)
+        {
+            while (start < end)
+            {
+                (array[start], array[end]) = (array[end], array[start]);
+                start++;
+                end--;
+            }
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day16/Challange2.cs
@@ -0,0 +1,88 @@
+﻿namespace advent_of_code.Year2017.Day16
+{
+    public static class Challange2
+    {
+        private static readonly int Count = 1_000_000_000;
+
+        public static string DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(',');
+
+            var state = "abcdefghijklmnop".ToCharArray();
+            var states = new List<string>();
+
+
+            for (var r = 0; r < Count; r++)
+            {
+                foreach (var line in input)
+                {
+                    var data = line[1..].Split('/');
+                    switch (line[0])
+                    {
+                        case 's':
+                            var len = int.Parse(data[0]);
+                            Spin(ref state, len);
+                            break;
+                        case 'x':
+                            var pos1 = int.Parse(data[0]);
+                            var pos2 = int.Parse(data[1]);
+                            (state[pos2], state[pos1]) = (state[pos1], state[pos2]);
+                            break;
+                        case 'p':
+                            var found = false;
+                            for (var i = 0; i < state.Length; i++)
+                            {
+                                if (state[i] == data[0][0])
+                                {
+                                    state[i] = data[1][0];
+                                    if (found) break;
+                                    found = true;
+                                }
+                                else if (state[i] == data[1][0])
+                                {
+                                    state[i] = data[0][0];
+                                    if (found) break;
+                                    found = true;
+                                }
+                            }
+                            break;
+                    }
+                }
+                var newState = new string(state);
+                if (!states.Contains(newState))
+                {
+                    states.Add(newState);
+                    continue;
+                }
+
+                var loopStart = states.IndexOf(newState);
+                var loopLen = states.Count - loopStart;
+                var remainder = (Count - loopStart) % (loopLen);
+                var position = loopStart + remainder - 1;
+                if (position < 0) position = states.Count - 1;
+                return states[position];
+            }
+
+            return new string(state);
+        }
+
+        private static void Spin(ref char[] array, int length)
+        {
+            var n = array.Length;
+            length %= n;
+            Reverse(ref array, 0, n - 1);
+            Reverse(ref array, 0, length - 1);
+            Reverse(ref array, length, n - 1);
+        }
+
+        private static void Reverse(ref char[] array, int start, int end)
+        {
+            while (start < end)
+            {
+                (array[start], array[end]) = (array[end], array[start]);
+                start++;
+                end--;
+            }
+        }
+    }
+}
