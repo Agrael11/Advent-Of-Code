c4f6ef49ba8d515f9149d3873411b4791aeff3ff
Tachi <Oliver.Neuschl@gmail.com>
2024-05-18 19:37:03 +0200

    Year 2017, Day 13

---
+++ advent_of_code.Year2017.Day13/Challange1.cs
@@ -0,0 +1,25 @@
+﻿namespace advent_of_code.Year2017.Day13
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var totalSeverity = 0;
+
+            foreach (var layer in input)
+            {
+                var split = layer.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                var layerNumber = int.Parse(split[0]);
+                var layerDepth = int.Parse(split[1]);
+                if (layerNumber % ((layerDepth * 2) - 2) == 0)
+                {
+                    totalSeverity += layerNumber*layerDepth;
+                }
+            }
+
+            return totalSeverity;
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day13/Challange2.cs
@@ -0,0 +1,36 @@
+﻿namespace advent_of_code.Year2017.Day13
+{
+    public static class Challange2
+    {
+        private static readonly List<(int number, int cycle)> Layers = new List<(int, int)>();
+
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+            Layers.Clear();
+
+            foreach (var layer in input)
+            {
+                var split = layer.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                var layerNumber = int.Parse(split[0]);
+                var layerDepth = int.Parse(split[1]);
+                Layers.Add((layerNumber, (layerDepth * 2) - 2));
+            }
+
+            var delay = 0;
+
+            for (; Caugh(delay); delay++) {; }
+
+            return delay;
+        }
+
+        public static bool Caugh(int delay)
+        {
+            foreach (var (number, cycle) in Layers)
+            {
+                if ((number + delay) % cycle == 0) return true;
+            }
+            return false;
+        }
+    }
+}
