06d46a708e35a3b83a566728b82f524812e0511a
Tachi <Oliver.Neuschl@gmail.com>
2024-05-19 15:37:35 +0200

    Year 2017, Day 17

---
+++ advent_of_code.Year2017.Day17/Challange1.cs
@@ -0,0 +1,24 @@
+﻿namespace advent_of_code.Year2017.Day17
+{
+    public static class Challange1
+    {
+        private static readonly int Count = 2017;
+
+        public static int DoChallange(string inputData)
+        {
+            var input = int.Parse(inputData.Replace("\r", "").TrimEnd('\n'));
+
+            var numbers = new List<int>(Count);
+            numbers.Add(0);
+            var pos = 0;
+
+            for (var i = 1; i <= Count; i++)
+            {
+                pos = (pos + input) % numbers.Count + 1;
+                numbers.Insert(pos, i);
+            }
+
+            return numbers[pos+1];
+        }
+    }
+}
---
+++ advent_of_code.Year2017.Day17/Challange2.cs
@@ -0,0 +1,25 @@
+﻿namespace advent_of_code.Year2017.Day17
+{
+    public static class Challange2
+    {
+        private static readonly int Count = 50_000_000;
+
+        public static int DoChallange(string inputData)
+        {
+            var input = int.Parse(inputData.Replace("\r", "").TrimEnd('\n'));
+
+            var length = 1;
+            var after0 = 0;
+            var pos = 0;
+
+            for (var i = 1; i <= Count; i++)
+            {
+                pos = (pos + input) % length + 1;
+                if (pos == 1) { after0 = i; }
+                length++;
+            }
+
+            return after0;
+        }
+    }
+}
