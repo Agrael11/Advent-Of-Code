d8c7b43c4b324317d7b761b6d773a7c0cb3ac903
Tachi <Oliver.Neuschl@gmail.com>
2024-06-01 14:43:41 +0200

    Year 2018, Day 1

---
+++ advent_of_code.Year2018.Day01/Challange1.cs
@@ -0,0 +1,19 @@
+﻿namespace advent_of_code.Year2018.Day01
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var result = 0;
+
+            foreach (var line in input)
+            {
+                result += int.Parse(line);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ advent_of_code.Year2018.Day01/Challange2.cs
@@ -0,0 +1,44 @@
+﻿namespace advent_of_code.Year2018.Day01
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var frequency = 0;
+            var frequencies = new HashSet<int>();
+            var instructions = new List<int>();
+            frequencies.Add(0);
+
+
+            foreach (var line in input)
+            {
+                var instruction = int.Parse(line);
+                frequency += instruction;
+                if (frequencies.Contains(frequency))
+                {
+                    return frequency;
+                }
+                instructions.Add(instruction);
+                frequencies.Add(frequency);
+            }
+
+            for (; ;)
+            {
+                foreach (var instruction in instructions)
+                {
+                    frequency += instruction;
+                    if (frequencies.Contains(frequency))
+                    {
+                        return frequency;
+                    }
+                    frequencies.Add(frequency);
+                }
+            }
+
+
+            return -1;
+        }
+    }
+}
