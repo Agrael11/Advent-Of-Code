ae464c730aed45c6ceb1163e6d5f3565cb03742f
Tachi <Oliver.Neuschl@gmail.com>
2024-06-06 13:05:53 +0200

    Year 2018, Day 2

---
+++ advent_of_code.Year2018.Day02/Challange1.cs
@@ -0,0 +1,35 @@
+﻿namespace advent_of_code.Year2018.Day02
+{
+    public static class Challange1
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            var threes = 0;
+            var twos = 0;
+
+            var counter = new Dictionary<char, int>();
+
+            foreach (var line in input)
+            {
+                counter.Clear();
+                foreach (var ch in line)
+                {
+                    if (counter.TryGetValue(ch, out var count))
+                    {
+                        counter[ch] = ++count;
+                    }
+                    else
+                    {
+                        counter[ch] = 1;
+                    }
+                }
+                if (counter.ContainsValue(3)) threes++;
+                if (counter.ContainsValue(2)) twos++;
+            }
+
+            return threes*twos;
+        }
+    }
+}
---
+++ advent_of_code.Year2018.Day02/Challange2.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace advent_of_code.Year2018.Day02
+{
+    public static class Challange2
+    {
+        public static string DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split('\n');
+
+            for (var index1 = 0; index1 < input.Length; index1++)
+            {
+                var box1 = input[index1];
+                for (var index2 = index1+1; index2 < input.Length; index2++)
+                {
+                    var box2 = input[index2];
+                    if (box1 == box2) continue;
+
+                    var result = FindDifferent(box1, box2);
+                    if (result != -1) return box1.Remove(result, 1);
+                }
+            }
+
+            return "";
+        }
+
+        public static int FindDifferent(string str1, string str2)
+        {
+            var diff = -1;
+            for (var indexC = 0; indexC < str1.Length; indexC++)
+            {
+                if (str1[indexC] != str2[indexC])
+                {
+                    if (diff != -1) return -1;
+                    diff = indexC;
+                }
+            }
+            if (diff != -1) return diff;
+            return -1;
+        }
+    }
+}
