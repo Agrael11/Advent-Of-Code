85ff5fca5aacfdd79b6b0469c216fd636a24f5dd
Tachi <Oliver.Neuschl@gmail.com>
2024-07-04 16:11:28 +0200

    Year 2018, Day 9

---
+++ advent_of_code.Year2018.Day09/Challange2.cs
@@ -0,0 +1,38 @@
+﻿namespace advent_of_code.Year2018.Day09
+{
+    public static class Challange2
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(' ');
+
+            var players = int.Parse(input[0]);
+            var maxMarble = int.Parse(input[6])*100;
+            var playerScores = new long[players];
+            var current = new LinkedMarble(0);
+            var currentPlayer = 0;
+            var maxScore = 0L;
+            for (var i = 1; i <= maxMarble; i++)
+            {
+                if (i % 23 == 0)
+                {
+                    playerScores[currentPlayer] += i;
+                    current = current.SevenBefore;
+                    playerScores[currentPlayer] += current.Value;
+
+                    maxScore = long.Max(playerScores[currentPlayer], maxScore);
+
+                    current.Remove();
+                    current = current.Next;
+                }
+                else
+                {
+                    current = current.Next.AddBehind(i);
+                }
+                currentPlayer = (currentPlayer + 1) % players;
+            }
+
+            return maxScore;
+        }
+    }
+}
---
+++ advent_of_code.Year2018.Day09/LinkedMarble.cs
@@ -0,0 +1,39 @@
+﻿namespace advent_of_code.Year2018.Day09
+{
+    internal class LinkedMarble
+    {
+        public int Value;
+        public LinkedMarble Next;
+        public LinkedMarble Previous;
+        public LinkedMarble SevenBefore => Previous.Previous.Previous.Previous.Previous.Previous.Previous;
+        public LinkedMarble(int value, LinkedMarble previous, LinkedMarble next)
+        {
+            Value = value;
+            Previous = previous;
+            Next = next;
+        }
+        public LinkedMarble(int value)
+        {
+            Value = value;
+            Next = this;
+            Previous = this;
+        }
+        public void Remove()
+        {
+            Previous.Next = Next;
+            Next.Previous = Previous;
+        }
+        public LinkedMarble AddBehind(int value)
+        {
+            var newMarble = new LinkedMarble(value, this, Next);
+            Next.Previous = newMarble;
+            Next = newMarble;
+            return newMarble;
+        }
+
+        public override string ToString()
+        {
+            return $"{Previous.Value} {{{Value}}} {Next.Value}";
+        }
+    }
+}
