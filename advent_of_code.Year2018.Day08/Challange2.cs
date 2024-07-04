947fad99293d95f3adb71ccfe870f04a64411de4
Tachi <Oliver.Neuschl@gmail.com>
2024-07-04 15:34:06 +0200

    Year 2018, Day 8

---
+++ advent_of_code.Year2018.Day08/Challange2.cs
@@ -0,0 +1,17 @@
+﻿namespace advent_of_code.Year2018.Day08
+{
+    public static class Challange2
+    {
+        public static int DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(' ');
+            var data = new List<int>();
+            foreach (var number in input)
+            {
+                data.Add(int.Parse(number));
+            }
+            var rootNode = Node.Parse(ref data, 0);
+            return rootNode.Value;
+        }
+    }
+}
---
+++ advent_of_code.Year2018.Day08/Node.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Year2018.Day08
+{
+    internal class Node
+    {
+        public readonly List<Node> Nodes = new List<Node>();
+        public readonly List<int> Metadata = new List<int>();
+        public readonly List<int> Data = new List<int>();
+        public int MetadataSum => Metadata.Sum() + Nodes.Sum(n => n.MetadataSum);
+        public int Value
+        {
+            get
+            {
+                if (Nodes.Count == 0)
+                {
+                    return Metadata.Sum();
+                }
+                else
+                {
+                    var sum = 0;
+                    foreach(var indexer in Metadata)
+                    {
+                        if (indexer <= Nodes.Count)
+                        {
+                            sum += Nodes[indexer-1].Value;
+                        }
+                    }
+                    return sum;
+                }
+            }
+        }
+
+        public static Node Parse(ref List<int> numbers, int position)
+        {
+            return Parse(ref numbers, ref position);
+        }
+        public static Node Parse(ref List<int> numbers, ref int position)
+        {
+            var node = new Node();
+            var nodeCount = numbers[position++];
+            var metadataCount = numbers[position++];
+            for (var i = 0; i < nodeCount; i++)
+            {
+                node.Nodes.Add(Parse(ref numbers, ref position));
+            }
+            for (var i = 0; i < metadataCount; i++)
+            {
+                node.Metadata.Add(numbers[position++]);
+            }
+            return node;
+        }
+    }
+}
