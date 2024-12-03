d94b06110b6a0a91ffc9f691ed6178be5c9a6dc8
Tachi <Oliver.Neuschl@gmail.com>
2024-12-03 16:45:05 +0100

    On the Third Day of Christmas Advent of Code 2024 Gave to me: Finding instructions in bunch of mess... Thanks Regex! *Fixed possible bug where 4 and more digits in row should not be counted, but were

--- advent_of_code.Year2024.Day03/Regexes.cs
+++ advent_of_code.Year2024.Day03/Regexes.cs
@@ -8,10 +8,10 @@ namespace advent_of_code.Year2024.Day03
         /// Only focuses on matching the "mul(number,number)"
         /// Finds string mul(
         /// Then it looks for any number of digits, character ',', again any number of digits and closing parenthesis - )
-        /// \d - digit, * = repeated any number of times
+        /// \d - digit, {1,3} = 1 to 3 times (so 1 to 3 digits one after another)
         /// \( and \) are just escaped parenthesis
         /// </summary>
-        [GeneratedRegex(@"mul\(\d*,\d*\)")]
+        [GeneratedRegex(@"mul\(\d{1,3},\d{1,3}\)")]
         internal static partial Regex RegexMuls();
 
         /// <summary>
@@ -19,7 +19,7 @@ namespace advent_of_code.Year2024.Day03
         /// Then it has two more literal groups - one for do() and one for don't()
         /// | is "OR" - allowing multiple groupss
         /// </summary>
-        [GeneratedRegex(@"(mul\(\d*,\d*\))|(do\(\))|(don't\(\))")]
+        [GeneratedRegex(@"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))")]
         internal static partial Regex RegexMulsDoDonts();
     }
 }
