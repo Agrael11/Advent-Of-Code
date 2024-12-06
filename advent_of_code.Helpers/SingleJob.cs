1dab7f9ca6e5a78f5b50d8178e07a53f363c465b
Tachi <Oliver.Neuschl@gmail.com>
2024-12-06 13:57:28 +0100

    Added job processing

--- advent_of_code.Helpers/SingleJob.cs
+++ advent_of_code.Helpers/SingleJob.cs
@@ -5,6 +5,33 @@
         public T? Result { get; private set; }
         public Func<T> Function { get; } = func;
 
+        public static void RunParallelized(List<SingleJob<T>> jobs)
+        {
+            Parallel.ForEach(jobs, job => job.Run());
+        }
+
+        public static List<BatchJob<T, ResultType>> RunParallelized<ResultType>(List<SingleJob<T>> jobs, int batchSize, Func<List<T?>, ResultType> countFunction)
+        {
+            var batches = new List<BatchJob<T, ResultType>>();
+            var batch = new BatchJob<T, ResultType>(countFunction);
+            foreach (var job in jobs)
+            {
+                batch.Jobs.Add(job);
+
+                if (batch.Size == batchSize)
+                {
+                    batches.Add(batch);
+                    batch = new BatchJob<T, ResultType>(countFunction);
+                }
+            }
+
+            if (batch.Size > 0) batches.Add(batch);
+            
+            BatchJob<T, ResultType>.RunMultipleParallelized(batches);
+            
+            return batches;
+        }
+
         public void Run()
         {
             Result = Function.Invoke();
