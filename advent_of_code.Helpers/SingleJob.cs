c700f2164232e19b4213e444882972b9e5a178d9
Tachi <Oliver.Neuschl@gmail.com>
2024-12-06 12:05:31 +0100

    Added Jobs

---
+++ advent_of_code.Helpers/SingleJob.cs
@@ -0,0 +1,13 @@
+﻿namespace advent_of_code.Helpers
+{
+    public class SingleJob<T> (Func<T> func)
+    {
+        public T? Result { get; private set; }
+        public Func<T> Function { get; } = func;
+
+        public void Run()
+        {
+            Result = Function.Invoke();
+        }
+    }
+}
