22c4ecafcafa598bdeed0fa5766d47e43e1f1d83
Tachi <Oliver.Neuschl@gmail.com>
2024-12-06 12:05:31 +0100

    Added Jobs

---
+++ advent_of_code.Helpers/BatchJob.cs
@@ -0,0 +1,17 @@
+﻿namespace advent_of_code.Helpers
+{
+    internal class BatchJob<SingleType,ResultType> (Func<List<SingleType?>, ResultType> countJob) 
+    {
+        public ResultType Results => countJob(Jobs.Select(job => job.Result).ToList());
+        public int Size => Jobs.Count;
+        public List<SingleJob<SingleType>> Jobs { get; } = new List<SingleJob<SingleType>>();
+
+        public void Run()
+        {
+            foreach (var job in Jobs)
+            {
+                job.Run();
+            }
+        }
+    }
+}
