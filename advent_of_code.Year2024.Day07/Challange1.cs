4e3d5d510dda60c28319d40993a8ce8babc758f7
Tachi <Oliver.Neuschl@gmail.com>
2024-12-07 22:51:10 +0100

    On the Seventh Day of Christmas, Advent of Code 2024 Gave to me: Equation checking without given operators... sounds like fun... really...
    *Made the algorithm work backwards.which makes it LOT faster

--- advent_of_code.Year2024.Day07/Challange1.cs
+++ advent_of_code.Year2024.Day07/Challange1.cs
@@ -9,35 +9,36 @@ namespace advent_of_code.Year2024.Day07
         {
             var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n");
 
-            //We create jobs - convert lines to equations and check them if they are okay. If they are - we take their result.
-            var jobs = input.Select(line =>
-                new SingleJob<long>(new Func<long>(() =>
-                {
-                    var equation = new Equation(line);
-                    var result = Common.IsOkay(equation, GetNext);
-                    return result ? equation.ExpectedResult : 0;
-                }))).ToList();
+            var total = 0L;
 
-            //We let jubs run, in batches of 100, which is arbitrary number that worked for me best. As result of them - we just sum them
-            var batches = SingleJob<long>.RunParallelized<long>(jobs, 100, (results) => results.Sum());
+            foreach (var equation in input.Select(line => new Equation(line)))
+            {
+                total += Common.IsOkay(equation, GetNext);
+            }
 
-            return batches.Sum(batch => batch.Results);
+            return total;
         }
 
         public static IEnumerable<(Equation, int, long)> GetNext((Equation equation, int index, long intermediateResult) state)
         {
-            if (state.index < state.equation.Numbers.Length) //If we have next number
+            if (state.index >= 0) //If we have next number
             {
-                //We get next number and get two results - one for addition and one for multiplication
-                var i1 = state.intermediateResult * state.equation.Numbers[state.index];
-                var i2 = state.intermediateResult + state.equation.Numbers[state.index];
-                if (i1 <= state.equation.ExpectedResult) //If we did not exceeded expected result by addition
+                //We get next number and get result of subtraction
+                var nextNumber = state.equation.Numbers[state.index];
+
+                var resultSubtraction = state.intermediateResult - nextNumber;
+                if (resultSubtraction >= 0) //If result is not negative, we return at possible subtraction
                 {
-                    yield return (state.equation, state.index + 1, i1);
+                    yield return (state.equation, state.index - 1, resultSubtraction);
                 }
-                if (i2 <= state.equation.ExpectedResult) //If we did not exceeded expected result by multiplication
+
+                //We also get result of division, and it's remainder
+                var resultDiv = state.intermediateResult / nextNumber;
+                var remainderDiv = state.intermediateResult % nextNumber;
+
+                if (remainderDiv == 0 && resultDiv >= 0) //If remainder is 0 and result is not negative we return as possible division
                 {
-                    yield return (state.equation, state.index + 1, i2);
+                    yield return (state.equation, state.index - 1, resultDiv);
                 }
             }
         }
--- advent_of_code.Year2024.Day07/Common.cs
+++ advent_of_code.Year2024.Day07/Common.cs
@@ -2,27 +2,27 @@
 {
     internal static class Common
     {
-        public static bool IsOkay(Equation equation, Func<(Equation, int, long), IEnumerable<(Equation, int, long)>> getNext)
+        public static long IsOkay(Equation equation, Func<(Equation, int, long), IEnumerable<(Equation, int, long)>> getNext)
         {
             //Simple DFS
             var stack = new Stack<(Equation, int, long)>();
-            stack.Push((equation, 1, equation.Numbers[0]));
+            stack.Push((equation, equation.Numbers.Length - 1, equation.ExpectedResult));
             while (stack.Count > 0)
             {
                 var current = stack.Pop();
-                if (IsEnd(current)) return true;
+                if (IsEnd(current)) return equation.ExpectedResult;
                 foreach (var next in getNext(current))
                 {
                     stack.Push(next);
                 }
             }
-            return false;
+            return 0;
         }
 
         public static bool IsEnd((Equation equation, int index, long intermidateResult) state)
         {
             //If we used all numbers and are equal to result we are at correct end
-            return (state.index == state.equation.Numbers.Length && state.intermidateResult == state.equation.ExpectedResult);
+            return (state.index == -1 && state.intermidateResult == 0);
         }
     }
 }
