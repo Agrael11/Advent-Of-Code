dc5d67ac3427322fb825d32a74da7d6646e4eec3
Tachi <Oliver.Neuschl@gmail.com>
2024-12-11 11:56:35 +0100

    On the Twelfth Day of Christmas, Advent of Code 2024 Gave to me: This annoying stone counthing challange

---
+++ advent_of_code.Year2024.Day11/Challange1.cs
@@ -0,0 +1,17 @@
+﻿namespace advent_of_code.Year2024.Day11
+{
+    public static class Challange1
+    {
+        public static readonly int BLINKS = 25;
+
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(" ").Select(ulong.Parse);
+
+            var numbers = input.ToDictionary(number => number, _ => 1L);
+
+            //Counts new stones added over 25 blinks period + original stone count
+            return Common.CountNewStonesOverBlink(BLINKS, numbers) + input.Count();
+        }
+    }
+}
---
+++ advent_of_code.Year2024.Day11/Challange2.cs
@@ -0,0 +1,16 @@
+﻿namespace advent_of_code.Year2024.Day11
+{
+    public static class Challange2
+    {
+        public static readonly int BLINKS = 75;
+
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(' ').Select(ulong.Parse);
+
+            var numbers = input.ToDictionary(number => number, _ => 1L);
+
+            return Common.CountNewStonesOverBlink(BLINKS, numbers) + input.Count();
+        }
+    }
+}
