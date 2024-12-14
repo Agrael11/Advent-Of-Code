3542bc00997819a37bcf8e81b8b29e50f9e723b4
Tachi <Oliver.Neuschl@gmail.com>
2024-12-14 15:31:42 +0100

    On the Fourteenth Day of Christmas, Advent of Code 2024 Gave to me: Robot moving challange. If I ussed arrays instead of "fancy" collections since start, this would be much faster done.

---
+++ advent_of_code.Year2024.Day14/Robot.cs
@@ -0,0 +1,29 @@
+﻿namespace advent_of_code.Year2024.Day14
+{
+    internal class Robot
+    {
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public int VectorX { get; private init; }
+        public int VectorY { get; private init; }
+
+        public Robot(string definition)
+        {
+            var definitionSplit = definition.Split(' ').Select(part => part[2..].Split(',').Select(int.Parse)).ToArray();
+            LocationX = definitionSplit[0].First();
+            LocationY = definitionSplit[0].Last();
+            VectorX = definitionSplit[1].First();
+            VectorY = definitionSplit[1].Last();
+        }
+
+        public void Move(int width, int height, int steps)
+        {
+            //This moves robot *steps* times by his vector. it also keeps him withing bounds by wrapping
+            var x = (LocationX + VectorX * steps) % width;
+            var y = (LocationY + VectorY * steps) % height;
+            if (x < 0) x += width;
+            if (y < 0) y += height;
+            (LocationX, LocationY) = (x, y); 
+        }
+    }
+}
