0aa79c2aab286eb599ab71153edb02289a425501
Tachi <Oliver.Neuschl@gmail.com>
2024-12-14 15:31:42 +0100

    On the Fourteenth Day of Christmas, Advent of Code 2024 Gave to me: Robot moving challange. If I ussed arrays instead of "fancy" collections since start, this would be much faster done.

---
+++ advent_of_code.Year2024.Day14/Challange1.cs
@@ -0,0 +1,70 @@
+﻿namespace advent_of_code.Year2024.Day14
+{
+    public static class Challange1
+    {
+        private static readonly int WIDTH = 101;
+        private static readonly int HEIGHT = 103;
+        private static readonly int SECONDS = 100;
+
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n");
+            var quadrants = new long[] { 0L, 0L, 0L, 0L };
+
+            foreach (var robot in input.Select(line => new Robot(line)))
+            {
+                //This moves robot 100 times by his vector
+                robot.Move(WIDTH, HEIGHT, SECONDS);
+
+                //We find in which quadrant he is
+                var quadrant = GetQuadrant(robot.LocationX, robot.LocationY);
+                
+                //If robot is in quadrant, we increase count of robots in it
+                if (quadrant >= 0)
+                {
+                    quadrants[quadrant]++;
+                }
+            }
+            
+            var total = 1L;
+            foreach (var quadrant in quadrants)
+            {
+                total *= quadrant;
+            }
+
+            return total;
+        }
+
+        private static int GetQuadrant(long X, long Y)
+        {
+            //This just checks in which quadrant robots iis
+            var middleColumn = WIDTH / 2;
+            var middleRow = HEIGHT / 2;
+
+            if (Y < middleRow) //Top half
+            {
+                if (X < middleColumn) //Top Left quadrant
+                {
+                    return 0;
+                }
+                else if (X > middleColumn) //Top Right quadrant
+                {
+                    return 1;
+                }
+            }
+            else if (Y > middleRow) //Bottom Half
+            {
+                if (X < middleColumn) //Bottom Left quadrant
+                {
+                    return 2;
+                }
+                else if (X > middleColumn) //Bottom Right quadrant
+                {
+                    return 3;
+                }
+            }
+
+            return -1; //In between quadrands. center lines don't count...
+        }
+    }
+}
---
+++ advent_of_code.Year2024.Day14/Challange2.cs
@@ -0,0 +1,66 @@
+﻿namespace advent_of_code.Year2024.Day14
+{
+    public static class Challange2
+    {
+        private static readonly int WIDTH = 101;
+        private static readonly int HEIGHT = 103;
+
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n");
+
+            var robots = input.Select(line => new Robot(line)).ToList();
+            var map = new bool[WIDTH, HEIGHT];
+            var rowLengths = new int[HEIGHT];
+
+            //Precompute count of robots in lines
+            foreach (var robot in robots)
+            {
+                rowLengths[robot.LocationY]++;
+            }
+
+            //We cound seconds until we find correct robot
+            for (var second = 1; ; second++)
+            {
+                map = new bool[WIDTH, HEIGHT];
+
+                //First we move all of them and update map and rowcounts
+                foreach (var robot in robots)
+                {
+                    rowLengths[robot.LocationY]--;
+
+                    //Inlining this makes it MUCH faster than calling Robot.Move() :P
+                    var x = (robot.LocationX + robot.VectorX) % WIDTH;
+                    var y = (robot.LocationY + robot.VectorY) % HEIGHT;
+                    if (x < 0) x += WIDTH;
+                    if (y < 0) y += HEIGHT;
+                    robot.LocationX = x;
+                    robot.LocationY = y;
+                    
+                    map[x, y] = true;
+                    rowLengths[y]++;
+                }
+                
+                //Then we search for line of robots
+                for (var y = 0; y < HEIGHT; y++)
+                {
+                    if (rowLengths[y] < 30) continue;
+
+                    var lineLength = 0;
+                    for (var x = 0L; x < WIDTH; x++)
+                    {
+                        if (map[x,y])
+                        {
+                            lineLength++;
+                            if (lineLength == 30) return second; //30 robots in line should mark correct one.
+                        }
+                        else
+                        {
+                            lineLength = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
