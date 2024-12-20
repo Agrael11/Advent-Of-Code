71c85a90f9450712dccfee545845b0825f814998
Tachi <Oliver.Neuschl@gmail.com>
2024-12-20 23:57:00 +0100

    Upgraded Trie +Added End Of Trie +Added Branch Keys Enumeration +Added Parent and Root

--- advent_of_code.Helpers/Trie.cs
+++ advent_of_code.Helpers/Trie.cs
@@ -1,9 +1,31 @@
 ﻿namespace advent_of_code.Helpers
 {
-    public class Trie<T>(T value) where T : notnull
+    public class Trie<T> where T : notnull
     {
-        public T Value { get; private set; } = value;
         private readonly Dictionary<T, Trie<T>> Branches = new Dictionary<T, Trie<T>>();
+
+
+        public T Value { get; private set; }
+        public Trie<T> Root { get; private set; }
+        public Trie<T>? Parent { get; private set; }
+        public bool IsEndOfTrie { get; set; }
+
+        public Trie(T value, Trie<T> root, Trie<T>? parent = null, bool end = false)
+        {
+            Value = value;
+            Root = root;
+            Parent = parent;
+            IsEndOfTrie = end;
+        }
+
+        public Trie(T value, Trie<T>? parent = null, bool end = false)
+        {
+            Value = value;
+            Root = this;
+            Parent = parent;
+            IsEndOfTrie = end;
+        }
+
 
         public bool TryGetBranch(T value, out Trie<T>? branch)
         {
@@ -15,11 +37,16 @@
             return Branches.ContainsKey(value);
         }
 
-        public Trie<T> GetOrAddBranch(T value)
+        public List<T> GetBranchKeys()
+        {
+            return Branches.Keys.ToList();
+        }
+
+        public Trie<T> GetOrAddBranch(T value, bool end = false)
         {
             if (Branches.TryGetValue(value, out var branch)) return branch;
 
-            branch = new Trie<T>(value);
+            branch = new Trie<T>(value, Root, this, end);
             Branches.Add(value, branch);
             return branch;
         }
