8d833b15a97fa433dc795b251a9d4ed4f5b536d6
Tachi <Oliver.Neuschl@gmail.com>
2024-12-22 10:43:04 +0100

    Added Caching Helper

---
+++ advent_of_code.Helpers/Cache.cs
@@ -0,0 +1,29 @@
+﻿namespace advent_of_code.Helpers
+{
+    public class Cache<T1, T2> where T1 : notnull
+    {
+        private readonly Dictionary<T1, T2> cacheData;
+
+        public Cache() 
+        {
+            cacheData = new Dictionary<T1, T2>();
+        }
+
+        public T2 TryGetResult(T1 key, Func<T1, T2> function)
+        {
+            if (cacheData.TryGetValue(key, out var result))
+            {
+                return result;
+            }
+
+            result = function.Invoke(key);
+            cacheData.Add(key, result);
+            return result;
+        }
+
+        public void Clear()
+        {
+            cacheData.Clear();
+        }
+    }
+}
