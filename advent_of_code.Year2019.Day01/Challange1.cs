60403fba74f258b04bc10750b63eaf9f5909ec71
Tachi <Oliver.Neuschl@gmail.com>
2025-09-26 16:55:36 +0200

    Year 2019, Day 1

---
+++ advent_of_code.Year2019.Day01/Challange1.cs
@@ -0,0 +1,12 @@
+﻿namespace advent_of_code.Year2019.Day01
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n");
+
+            return input.Select(t => Common.CalculateFuel(int.Parse(t), false)).Sum();
+        }
+    }
+}
---
+++ advent_of_code.Year2019.Day01/Common.cs
@@ -0,0 +1,11 @@
+﻿namespace advent_of_code.Year2019.Day01
+{
+    internal class Common
+    {
+        public static int CalculateFuel(int mass, bool recursive)
+        {
+            var fuel = mass / 3 - 2;
+            return (fuel < 0) ? 0 : ((recursive) ? (fuel + CalculateFuel(fuel, true)) : fuel);
+        }
+    }
+}
