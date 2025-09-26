78986ce52f527aa239b5221bcfeff6a2320c7d9e
Tachi <Oliver.Neuschl@gmail.com>
2025-09-26 18:31:53 +0200

    Initial IntMachine Implementation

---
+++ IntMachine/IntMachine.cs
@@ -0,0 +1,75 @@
+﻿using System.Reflection.Emit;
+using System.Reflection.Metadata.Ecma335;
+
+namespace IntMachine
+{
+    public class Machine
+    {
+        public enum RunResult { Okay, Exit, IndexOutOfRange, WrongOpcode, UnsupportedOpcode }
+
+        private readonly Dictionary<int, Func<RunResult>> OpcodeMap = new Dictionary<int, Func<RunResult>>();
+
+        public Memory RAM;
+        public int PC { get; set; }
+
+        public Machine()
+        {
+            RAM = new Memory(0);
+            PC = 0;
+            OpcodeMap.Add(1, OpcodeAdd);
+            OpcodeMap.Add(2, OpcodeMul);
+            OpcodeMap.Add(99, OpcodeExit);
+        }
+
+
+        public RunResult Run(params List<int> supportedOpcodes)
+        {
+            while (true)
+            {
+                var result = Step(supportedOpcodes);
+                if (result != RunResult.Okay) return result;
+            }
+        }
+
+        public RunResult Step(params List<int> supportedOpcodes)
+        {
+            if (!RAM.TryRead(PC, out var opcode) || opcode is null) return RunResult.IndexOutOfRange;
+            if (!supportedOpcodes.Contains(opcode.Value)) return RunResult.UnsupportedOpcode;
+            if (!OpcodeMap.TryGetValue(opcode.Value, out var fuction) || fuction is null) return RunResult.WrongOpcode;
+            return fuction.Invoke();
+        }
+
+        private RunResult OpcodeAdd()
+        {
+            var memAddr = PC;
+            PC += 4;
+            if (!RAM.TryReadIndirect(memAddr+1, out var value1) ||
+                !RAM.TryReadIndirect(memAddr+2, out var value2) ||
+                value1 is null || value2 is null ||
+                !RAM.TryWriteIndirect(memAddr+3, value1.Value + value2.Value))
+            {
+                return RunResult.IndexOutOfRange;
+            }
+            return RunResult.Okay;
+        }
+
+        private RunResult OpcodeMul()
+        {
+            var memAddr = PC;
+            PC += 4;
+            if (!RAM.TryReadIndirect(memAddr + 1, out var value1) ||
+                !RAM.TryReadIndirect(memAddr + 2, out var value2) ||
+                value1 is null || value2 is null ||
+                !RAM.TryWriteIndirect(memAddr + 3, value1.Value * value2.Value))
+            {
+                return RunResult.IndexOutOfRange;
+            }
+            return RunResult.Okay;
+        }
+
+        private RunResult OpcodeExit()
+        {
+            return RunResult.Exit;
+        }
+    }
+}
---
+++ IntMachine/Memory.cs
@@ -0,0 +1,68 @@
+﻿namespace IntMachine
+{
+    public class Memory(int size)
+    {
+        private int[] Data = new int[size];
+
+        public bool TryRead(int pointer, out int? result)
+        {
+            if (pointer < 0 || pointer >= Data.Length)
+            {
+                result = null;
+                return false;
+            }
+
+            result = Data[pointer];
+            return true;
+        }
+
+        public bool TryWrite(int pointer, int value)
+        {
+            if (pointer < 0 || pointer >= Data.Length)
+            {
+                return false;
+            }
+
+            Data[pointer] = value;
+            return true;
+        }
+
+        public bool TryReadIndirect(int pointer, out int? result)
+        {
+            if (!TryRead(pointer, out var indirect) || indirect is null)
+            {
+                result = null;
+                return false;
+            }
+
+            return TryRead(indirect.Value, out result);
+        }
+
+        public bool TryWriteIndirect(int pointer, int value)
+        {
+            if (!TryRead(pointer, out var indirect) || indirect is null)
+            {
+                return false;
+            }
+
+            return TryWrite(indirect.Value, value);
+        }
+
+
+
+
+        public static explicit operator Memory(int[] data)
+        {
+            var mem = new Memory(data.Length)
+            {
+                Data = (int[])data.Clone()
+            };
+            return mem;
+        }
+
+        public static explicit operator int[](Memory mem)
+        {
+            return mem.Data;
+        }
+    }
+}
