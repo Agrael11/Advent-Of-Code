833138af67f01849261993597971c7e80a0ec446
Tachi <Oliver.Neuschl@gmail.com>
2025-09-27 14:06:40 +0200

    Year 2019, Day 4

---
+++ advent_of_code.Year2019.Day04/Challange1.cs
@@ -0,0 +1,37 @@
+﻿namespace advent_of_code.Year2019.Day04
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("-").Select(int.Parse).ToArray();
+
+            var validPasswords = 0;
+
+            for (var i = input[0]; i <= input[1]; i++)
+            {
+                validPasswords += IsValid(i) ? 1 : 0;
+            }
+
+            return validPasswords;
+        }
+
+        public static bool IsValid(int password)
+        {
+            var tmpPas = password;
+            var lastDigit = tmpPas % 10;
+            tmpPas /= 10;
+            var twoSame = false;
+            while (tmpPas != 0)
+            {
+                var newLast = tmpPas % 10;
+                if (newLast > lastDigit) return false;
+                if (newLast == lastDigit) twoSame = true;
+                lastDigit = newLast;
+                tmpPas /= 10;
+            }
+
+            return twoSame;
+        }
+    }
+}
---
+++ advent_of_code.Year2019.Day04/Challange2.cs
@@ -0,0 +1,51 @@
+﻿namespace advent_of_code.Year2019.Day04
+{
+    public static class Challange2
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("-").Select(int.Parse).ToArray();
+
+            IsValid(123444);
+
+            var validPasswords = 0;
+
+            for (var i = input[0]; i <= input[1]; i++)
+            {
+                validPasswords += IsValid(i) ? 1 : 0;
+            }
+
+            return validPasswords;
+        }
+
+        public static bool IsValid(int password)
+        {
+            var tmpPas = password;
+            var lastDigit = tmpPas % 10;
+            tmpPas /= 10;
+            var twoSame = false;
+            var repeats = 1;
+            while (tmpPas != 0)
+            {
+                var newLast = tmpPas % 10;
+                if (newLast > lastDigit) return false;
+                
+                if (newLast == lastDigit)
+                {
+                    repeats++;
+                }
+                else
+                {
+                    if (repeats == 2) twoSame = true;
+                    repeats = 1;
+                }
+
+                lastDigit = newLast;
+                tmpPas /= 10;
+            }
+            if (repeats == 2) twoSame = true;
+
+            return twoSame;
+        }
+    }
+}
