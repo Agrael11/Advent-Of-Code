695a616c40e59df632ccfbc598a9466abec3cf3d
Tachi <Oliver.Neuschl@gmail.com>
2025-09-27 17:51:31 +0200

    Year 2019, Day 5

---
+++ advent_of_code.Year2019.Day05/Challange1.cs
@@ -0,0 +1,40 @@
+﻿using IntMachine;
+
+namespace advent_of_code.Year2019.Day05
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split(",").Select(int.Parse).ToArray();
+
+            var machine = new Machine
+            {
+                RAM = (Memory)input
+            };
+
+            machine.PushInput(1);
+            var result = machine.Run([1, 2, 3, 4, 99]);
+            if (result != Machine.RunResult.Exit)
+            {
+                throw new Exception($"IntMachine error: {Enum.GetName(result)}");
+            }
+
+            int? output = null;
+            while (machine.OutputAvailable())
+            {
+                if (!machine.TryPopOutput(out output) || output is null)
+                {
+                    throw new Exception("Error popping");
+                }
+            }
+            
+            if (output is null)
+            {
+                throw new Exception("No output found");
+            }
+
+            return output.Value;
+        }
+    }
+}
