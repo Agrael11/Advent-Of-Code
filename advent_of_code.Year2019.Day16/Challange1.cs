bc8b1041205f8aa6c77ef29e29ab6214ba53b658
Tachi <Oliver.Neuschl@gmail.com>
2025-10-09 15:25:10 +0200

    Year 2019, Day 16 - Flawed Frequency Transmission I hate this

---
+++ advent_of_code.Year2019.Day16/Challange1.cs
@@ -0,0 +1,57 @@
+﻿namespace advent_of_code.Year2019.Day16
+{
+    public static class Challange1
+    {
+        private static readonly int[] BasePattern = [0, 1, 0, -1];
+
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").Replace("\n","").Select(t=>int.Parse(t.ToString())).ToArray();
+
+            return long.Parse(string.Join("",DoPhases(input, 100).Take(8)));
+        }
+
+        public static int[] DoPhases(int[] input, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                input = DoThePhase(input);
+            }
+
+            return input;
+        }
+
+        private static int[] DoThePhase(int[] inputSignal)
+        {
+            var length = inputSignal.Length;
+            var result = new int[length];
+            for (var targetPosition = 0; targetPosition < length; targetPosition++)
+            {
+                var intResult = 0;
+                for (var inputPosition = 0; inputPosition < length; inputPosition++)
+                {
+                    var patternPoint = GetItemInPattern(inputPosition, targetPosition);
+                    if (patternPoint == 0) continue;
+                    var item = inputSignal[inputPosition];
+                    if (item == 0) continue;
+                    intResult += item * patternPoint;
+                }
+                if (intResult < 0)
+                {
+                    result[targetPosition] = Math.Abs(intResult) % 10;
+                }
+                else
+                {
+                    result[targetPosition] = intResult % 10;
+                }
+            }
+            return result;
+        }
+
+        private static int GetItemInPattern(int inputPosition, int outputPosition)
+        {
+            var realPos = (inputPosition + 1) / (outputPosition + 1);
+            return BasePattern[realPos % BasePattern.Length];
+        }
+    }
+}
---
+++ advent_of_code.Year2019.Day16/Challange2.cs
@@ -0,0 +1,40 @@
+﻿namespace advent_of_code.Year2019.Day16
+{
+    public static class Challange2
+    {
+        public static string DoChallange(string inputData)
+        {
+            var input= inputData.Replace("\r", "").Replace("\n", "")
+                .Select(t => int.Parse(t.ToString())).ToArray();
+
+            var skip = int.Parse(inputData[..7]);
+            
+            var data = PrepareData(input, skip);
+
+            for (var i = 0; i < 100; i++)
+            {
+                Sum(ref data);
+            }
+
+            return string.Join("", data[0..8]);
+        }
+
+        private static int[] PrepareData(int[] input, int skip)
+        {
+            var result = new int[input.Length * 10000 - skip];
+            for (var i = 0; i < result.Length; i++)
+            {
+                result[i] = input[(i + skip )% input.Length];
+            }
+            return result;
+        }
+
+        private static void Sum(ref int[] data)
+        {
+            for (var i = data.Length - 2; i >= 0; i--)
+            {
+                data[i] = (data[i] + data[i + 1]) % 10;
+            }
+        }
+    }
+}
