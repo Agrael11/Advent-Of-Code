fc9bf707e936236f7a5ddc483bd17d8116c15917
Tachi <Oliver.Neuschl@gmail.com>
2025-11-26 15:35:06 +0100

    Added Direct Console Launcher for Easier Testing

---
+++ advent_of_code.ConsoleShortcut/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace advent_of_code.ConsoleShortcut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleOnly.AdventOfCode.Register();
+            Visualizers.AOConsole.Clear();
+            ConsoleOnly.AdventOfCode.Main();
+        }
+    }
+}
