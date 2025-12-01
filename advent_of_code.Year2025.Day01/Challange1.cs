d56b10db589b76229d001ffb3ccf8626729e919b
Tachi <Oliver.Neuschl@gmail.com>
2025-12-01 22:12:34 +0100

    2025: On the First day of Christmas, Advent of Code gave to me - this weird safe cracking minigame (which I managed to mess up lol)

---
+++ advent_of_code.Year2025.Day01/Challange1.cs
@@ -0,0 +1,29 @@
+﻿namespace advent_of_code.Year2025.Day01
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n");
+
+            //Dial starts at 50, password at 0
+            var dial = 50;
+            var password = 0;
+
+
+            foreach (var instruction in input)
+            {
+                //We read the number from instrution, and make it negative if instruction s tarts with "L" (rotate left)
+                var modifier = int.Parse(instruction[1..]) % 100;
+                if (instruction.StartsWith('L')) modifier *= -1;
+
+                //Normalize the number on dial
+                dial = (dial + modifier + 100) % 100;
+                //And if it's 0 we increment password
+                if (dial == 0) password++;
+            }
+
+            return password;
+        }
+    }
+}
---
+++ advent_of_code.Year2025.Day01/Challange2.cs
@@ -0,0 +1,39 @@
+﻿namespace advent_of_code.Year2025.Day01
+{
+    public static class Challange2
+    {
+        public static long DoChallange(string inputData)
+        {
+            //inputData = "L68\r\nL30\r\nR48\r\nL5\r\nR60\r\nL55\r\nL1\r\nL99\r\nR14\r\nL82";
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n");
+            
+            //We set this up and start same as in part 1
+            var dial = 50;
+            var password = 0;
+
+            foreach (var line in input)
+            {
+                var modifier = int.Parse(line[1..]);
+
+                //This time we first check number of times it passes through 0
+                if (line.StartsWith('L'))
+                {
+                    //For going left we use the remaining number to 100 from dial
+                    password += ((100 - dial)%100 + modifier) / 100;
+                    modifier %= 100;
+                    modifier *= -1;
+                }
+                else
+                {
+                    //For going left we use the remaining dial itself
+                    password += (dial + modifier) / 100;
+                    modifier %= 100;
+                }
+
+                dial = (dial + modifier + 100) % 100;
+            }
+            
+            return password;
+        }
+    }
+}
