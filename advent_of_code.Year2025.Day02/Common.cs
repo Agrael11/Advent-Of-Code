cb2d72253369248a750829051b2d6c761b95a6b2
Tachi <Oliver.Neuschl@gmail.com>
2025-12-02 10:58:49 +0100

    2025: On the Second day of Christmas, Advent of Code gave to me - this simple pattern checking in numbers (which I managed to mess up again lol)

---
+++ advent_of_code.Year2025.Day02/Common.cs
@@ -0,0 +1,10 @@
+﻿namespace advent_of_code.Year2025.Day02
+{
+    internal class Common
+    {
+        public static int CountDigits(long digit)
+        {
+            return digit.ToString().Length;
+        }
+    }
+}
