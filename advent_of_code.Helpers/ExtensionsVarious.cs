97c5f968e5c328b512168f90e6eb1d1ee7831cad
Tachi <Oliver.Neuschl@gmail.com>
2025-12-04 10:55:40 +0100

    Just a small extension for removing items from ienumerables & set.

---
+++ advent_of_code.Helpers/ExtensionsVarious.cs
@@ -0,0 +1,17 @@
+﻿namespace advent_of_code.Helpers
+{
+    public static class ExtensionsVarious
+    {
+        public static int RemoveAllWhere<T>(this HashSet<T> set, Predicate<T> match)
+        {
+            var removed = 0;
+            var lastRemoved = 0;
+            do
+            {
+                lastRemoved = set.RemoveWhere(match);
+                removed += lastRemoved;
+            } while (lastRemoved != 0);
+            return removed;
+        }
+    }
+}
