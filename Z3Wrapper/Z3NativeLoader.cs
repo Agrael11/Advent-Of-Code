3f68ee460eed287466f1937d38a0d99a7eaa4a4e
Tachi <Oliver.Neuschl@gmail.com>
2025-12-10 18:09:17 +0100

    Added binaries, now supports all platforms

--- Z3Wrapper/Z3NativeLoader.cs
+++ Z3Wrapper/Z3NativeLoader.cs
@@ -11,20 +11,64 @@ namespace Z3Wrapper
         public static void Load()
         {
             if (loaded) return;
+            string? name = null;
+            string? arch = null;
+            if (OperatingSystem.IsAndroid())
+            {
+                name = "libz3.so";
+                arch = "arm64_v8a";
+            }
+            else if (OperatingSystem.IsWindows())
+            {
+                name = "libz3.dll";
+                if (RuntimeInformation.OSArchitecture == Architecture.Arm64)
+                {
+                    arch = "win_arm64";
+                }
+                else if (RuntimeInformation.OSArchitecture == Architecture.X86)
+                {
+                    arch = "win_x86";
+                }
+            }
+            else if (OperatingSystem.IsLinux())
+            {
+                name = "libz3.so";
+                if (RuntimeInformation.OSArchitecture == Architecture.X64)
+                {
+                    arch = "linux_x64";
+                }
+                else if (RuntimeInformation.OSArchitecture == Architecture.Arm64)
+                {
+                    arch = "linux_arm64";
+                }
+                else if (RuntimeInformation.OSArchitecture == Architecture.X86)
+                {
+                    arch = "linux_x86";
+                }
+            }
 
+            if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrEmpty(arch))
+            {
+                Load(name, arch);
+            }
+        }
+
+        private static void Load(string name, string arch)
+        {
             // Pick your embedded resource here
             // e.g. "Z3Wrapper.Assets.linux-arm64.libz3.so"
             var resName = typeof(Z3NativeLoader).Assembly
                 .GetManifestResourceNames()
-                .First(n => n.EndsWith("libz3.so"));
+                .First(n => n.Contains(arch, StringComparison.OrdinalIgnoreCase) &&
+                n.EndsWith(name, StringComparison.OrdinalIgnoreCase));
 
             extractedPath = Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                "libz3.so");
+                name);
 
-            using (var s = typeof(Z3NativeLoader).Assembly.GetManifestResourceStream(resName))
-            using (var f = File.Create(extractedPath))
-                s.CopyTo(f);
+            using var s = typeof(Z3NativeLoader).Assembly.GetManifestResourceStream(resName) ?? throw new Exception("Resource load failed.");
+            using var f = File.Create(extractedPath);
+            s.CopyTo(f);
 
             NativeLibrary.SetDllImportResolver(
                 typeof(Microsoft.Z3.Context).Assembly,
