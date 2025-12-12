be66ab6ba99e78f3e50d4af16dbd082bccd1e936
Tachi <Oliver.Neuschl@gmail.com>
2025-12-12 13:02:55 +0100

    2025: On the Twelfth day of Christmas, Advent of Code gave to me: This shape packing problem... Which can be ignored. (I hope when fast solution works on all real inputs and not on example input)

---
+++ advent_of_code.Year2025.Day12/Challange1.cs
@@ -0,0 +1,50 @@
+﻿namespace advent_of_code.Year2025.Day12
+{
+    public static class Challange1
+    {
+        public static long DoChallange(string inputData)
+        {
+            var input = inputData.Replace("\r", "").TrimEnd('\n').Split("\n")
+                .Where(t => t.Contains('x')).ToList();
+
+
+            //I'm not proud of this. it's stupid that it works. I have no idea why it works
+            //But it works.
+            //Shape doesn't matter for some reason. even if it absolutely should.
+
+            return input.Count(FitsGifts);
+        }
+
+
+        /// <summary>
+        /// Checks if area fits all christmas gifts... pretending all are 3x3.
+        /// Even if they're not... it doesn't matter
+        /// </summary>
+        /// <param name="areaDefition"></param>
+        /// <returns></returns>
+        private static bool FitsGifts(string areaDefition)
+        {
+            var split = areaDefition.Split(':', StringSplitOptions.TrimEntries);
+            var area = split[0].Split('x').Select(int.Parse).ToArray();
+            var shapesCount = split[1].Split(' ').Select(int.Parse).Sum();
+
+            var neededArea = GetNormalizedArea(area[0], area[1]);
+            var shapeArea = shapesCount * 9;
+
+            return shapeArea <= neededArea;
+        }
+
+        /// <summary>
+        /// Calcualtes normalized area - where it is divisible by maximum gift size - 3x3
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        private static int GetNormalizedArea(int width, int height)
+        {
+            width -= width % 3;
+            height-= height% 3;
+            return width * height;
+        }
+    }
+}
---
+++ advent_of_code.Year2025.Day12/Challange2.cs
@@ -0,0 +1,12 @@
+﻿namespace advent_of_code.Year2025.Day12
+{
+    public static class Challange2
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static string DoChallange(string inputData)
+        {
+            return "Remaining stars were brought by elves";
+        }
+#pragma warning restore IDE0060 // Remove unused parameter
+    }
+}
